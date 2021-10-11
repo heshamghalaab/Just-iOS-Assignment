17f8de47983c45813b6ae3fcc0c3b27e9e5bd704
heshamghalaab <hesham_ghalaab@hotmail.com>
2021-10-11 22:44:29 +0200

    initial commit

---
+++ Just iOS Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Just iOS Assignment
+//
+//  Created by Ghalaab on 11/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
